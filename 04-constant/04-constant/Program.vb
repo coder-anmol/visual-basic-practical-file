e74702ab92698ff9526fd8a30c10dbccf16d1719
coder-anmol <dev.anmolsharma@gmail.com>
2022-02-27 03:18:57 +0530

    04-constant completed

---
+++ 04-constant/04-constant/Program.vb
@@ -0,0 +1,12 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Const constant As String = "This is a constant"
+
+        ' constant = "kdjskfjkdsfjk" : constant cannot be the target of assignment
+
+        Console.WriteLine(constant)
+
+    End Sub
+End Module

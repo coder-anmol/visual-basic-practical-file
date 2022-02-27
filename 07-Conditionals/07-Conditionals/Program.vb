8c75f175f2132745abdea4bcce5dd9696ee698f9
coder-anmol <dev.anmolsharma@gmail.com>
2022-02-27 16:34:56 +0530

    07-conditional-statements done

---
+++ 07-Conditionals/07-Conditionals/Program.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        'conditional statements
+        Dim condition As Boolean = True
+
+        If condition Then
+            Console.WriteLine("Condition is True")
+        Else
+            Console.WriteLine("Condition is False")
+        End If
+    End Sub
+End Module

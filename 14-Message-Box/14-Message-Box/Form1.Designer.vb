0db6f1e851538f5c33b611b2e72ace5309f5179d
coder-anmol <dev.anmolsharma@gmail.com>
2022-02-28 05:39:31 +0530

    14-Msg-Box done

---
+++ 14-Message-Box/14-Message-Box/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SayHelloButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'SayHelloButton
+        '
+        Me.SayHelloButton.Font = New System.Drawing.Font("Segoe UI", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.SayHelloButton.Location = New System.Drawing.Point(294, 140)
+        Me.SayHelloButton.Name = "SayHelloButton"
+        Me.SayHelloButton.Size = New System.Drawing.Size(190, 78)
+        Me.SayHelloButton.TabIndex = 0
+        Me.SayHelloButton.Text = "Say Hello"
+        Me.SayHelloButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SayHelloButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SayHelloButton As Button
+End Class
---
+++ 14-Message-Box/14-Message-Box/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub SayHelloButton_Click(sender As Object, e As EventArgs) Handles SayHelloButton.Click
+        MsgBox("Hello! From Visual Basic")
+    End Sub
+End Class

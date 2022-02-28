002e6f78ee9e514b574f60fa6a06534bf5393310
coder-anmol <dev.anmolsharma@gmail.com>
2022-02-28 05:59:16 +0530

    15-InputBox done

---
+++ 15-InputBox/15-InputBox/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim input As String = Nothing
+        input = InputBox("Enter your name: ")
+
+        If input <> "" Then
+            MsgBox("Hello! " & input)
+        Else
+            MsgBox("No input given")
+        End If
+
+        Me.Close()
+    End Sub
+End Class
